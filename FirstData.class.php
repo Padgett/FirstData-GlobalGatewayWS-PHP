daa024fd9c060d4b62d140d2de08b511e7237f17
Joshua Padgett <darkland.zhul@gmail.com>
2012-11-27 09:22:59 -0700

    Incremental - Generating post string, minor tweaks.

--- FirstData.class.php
+++ FirstData.class.php
@@ -8,10 +8,11 @@
  * 
  */
 class FirstData {
-  public $oid;
   private $sharedKey;
   private $store;
   private $postingURL;
+  private $testing;
+  public $oid;
   private $config;
   private $options;
   private $totals;
@@ -22,9 +23,11 @@ class FirstData {
    * Constructs the object.
    * Options array will be appended to the charge request. Add your custom fields here.
    * Required Params: sharedKey, store, oid(public - can be set later)
-   * Optional Params: config(array), options(array)
+   * Optional Params: oid, config(array), options(array)
+   * 
+   * txtntype Options: ECI, MOTO, RETAIL
    */
-  public function __construct($sharedKey,$store,$postingURL,$oid = '',$config = array(),$options = array()) {
+  public function __construct($sharedKey,$store,$postingURL,$testing = false,$oid = '',$config = array(),$options = array()) {
     if (!$sharedKey) {
       throw new Exception('Shared Key Required.');
     } else {
@@ -41,18 +44,18 @@ class FirstData {
       $this->postingURL = $postingURL;
     }
     
+    $this->testing = $testing;
+    
     if ($oid) $this->oid = $oid;
     
     $this->config = array(
-        'txtntype'    => $config['txtntype'] || 'sale',
-        'timezone'    => $config['timezone'] || date('T'),
-        'mode'        => $config['mode'] || 'payonly',
-        'trxOrigin'   => $config['trxOrigin'] || 'ECI'
+        'txtntype'    => (empty($config['txtntype'])) ? 'sale' : $config['txtntype'],
+        'timezone'    => (empty($config['timezone'])) ? date('T') : $config['timezone'],
+        'mode'        => (empty($config['mode'])) ? 'payonly' : $config['mode'],
+        'trxOrigin'   => (empty($config['trxOrigin'])) ? 'ECI' : $config['trxOrigin']
     );
     
-    foreach ($options as $key => $val) {
-      $this->options[$key] = $val;
-    }
+    if ($options) $this->setOptions($options);
   }
   
   public function __destruct() {
@@ -100,15 +103,28 @@ class FirstData {
     
     //Setup the billing info if it's been passed in.
     if ($billingInfo) {
-      //TODO: This.
+      $this->setBillingInfo($billingInfo);
     }
   }
   
   /* function: setBillingInfo
    * 
    */
-  public function setBillingInfo($info) {
-    //TODO: This.
+  public function setBillingInfo($billingInfo) {
+    $this->billingInfo = array(
+        'bcompany'  => $billingInfo['company'],
+        'bname'     => $billingInfo['name'],
+        'baddr1'    => $billingInfo['addr1'],
+        'baddr2'    => $billingInfo['addr2'],
+        'bcity'     => $billingInfo['city'],
+        'bstate'    => $billingInfo['state'],
+        'bstate2'   => $billingInfo['state2'],
+        'bcountry'  => $billingInfo['country'],
+        'bzip'      => $billingInfo['zip'],
+        'phone'     => $billingInfo['phone'],
+        'fax'       => $billingInfo['fax'],
+        'email'     => $billingInfo['email']
+    );
   }
   
   /* function: chargeIt
@@ -119,12 +135,62 @@ class FirstData {
       throw new Exception('Order ID (oid) is not set.');
     }
     
-    $txndatetime = date('%Y:%m:%d-%H:%i:%s');
-    $hash = createHash($txndatetime);
+    $txndatetime = date('Y:m:d-H:i:s');
+    $hash = $this->createHash($txndatetime);
     
     /*** Send to First Data ***/
+    //Gather all our arrays together
+    $fields['storename'] = $this->store;
+    $fields['oid'] = $this->oid;
+    $fields['txndatetime'] = $txndatetime;
+    $fields['hash'] = $hash;
+    foreach ($this->config as $key => $val) {
+      $fields[$key] = urlencode($val);
+    }
+    foreach ($this->options as $key => $val) {
+      $fields[$key] = urlencode($val);
+    }
+    foreach ($this->totals as $key => $val) {
+      $fields[$key] = urlencode($val);
+    }
+    foreach ($this->cardInfo as $key => $val) {
+      $fields[$key] = urlencode($val);
+    }
+    foreach ($this->billingInfo as $key => $val) {
+      $fields[$key] = urlencode($val);
+    }
+    
+    //url-ify the data for the POST
+    $fields_string = '';
+    foreach ($fields as $key=>$value) { 
+      $fields_string .= $key.'='.$value.'&';
+    }
+    $fields_string = rtrim($fields_string, '&');
+    //die($fields_string);
+
+    $ch = curl_init($this->postingURL);
+    //curl_setopt($ch, CURLOPT_HEADER, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    //curl_setopt($ch, CURLOPT_CAINFO, getcwd().'/connect.firstdataglobalgateway.com.pem');
+    if ($this->testing) curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+    $response = curl_exec($ch);
+    if($response === false)
+    {
+      throw new Exception('Curl error: '.curl_error($ch));
+    }
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
     
     /*** Handle Response ***/
+    if ($httpCode >= 400) {
+      throw new Exception('HTTP Error: '.$httpCode);
+    }
+    //TODO: Actually handle this properly
+    return $response; //For Testing
+    
+    
     
     //Let's wipe the card info from memory, just to be safe.
     $this->cardInfo = null;
@@ -144,7 +210,7 @@ class FirstData {
   /* function: addOptions
    * Appends to or updates existing options
    */
-  public function addOptions($options = array()) {
+  public function addOptions($options) {
     foreach ($options as $key => $val) {
       $this->options[$key] = $val;
     }
@@ -155,8 +221,9 @@ class FirstData {
    */
   private function createHash($dateTime) {
     $str = $this->store.$dateTime.$this->totals['chargetotal'].$this->sharedKey;
+    $hex_str = '';
     for ($i = 0; $i < strlen($str); $i++){ 
-      $hex_str.=dechex(ord($str[$i]));
+      $hex_str .= dechex(ord($str[$i]));
     }
     return hash('sha256', $hex_str);
   }
--- example.php
+++ example.php
@@ -1,6 +1,19 @@
 <?php
 require_once('config.php');
+require_once('FirstData.class.php');
 
-$cc = new FirstData($sharedKey, $store, $postingURL);
+$cc = new FirstData($sharedKey, $store, $postingURL, true, 1);
 
+$cc->setCardInfo('V', '4111111111111111', '12', '2014', '123');
+$cc->setTotals(1.00);
+$cc->setBillingInfo(array(
+    'name'    => 'Test Name',
+    'addr1'   => '123 Fake St.',
+    'city'    => 'City',
+    'state'   => 'AZ',
+    'country' => 'US',
+    'zip'     => '85080'
+));
+
+echo $cc->chargeIt();
 ?>
